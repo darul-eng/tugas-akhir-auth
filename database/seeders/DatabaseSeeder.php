5fd24814e6ccdaa78dfd9bbd705893f42ae0883c
darul-eng <ikhsand18d@student.unhas.ac.id>
2022-09-12 22:03:29 +0800

    menambha seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'nama' => 'Test User',
-            'email' => 'test@example.com',
+            'nama' => 'Tester',
+            'email' => 'test@gmail.com',
         ]);
     }
 }
